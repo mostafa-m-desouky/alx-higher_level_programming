e070edfbb7008b324c40cecfb93cc4910c940d25
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-06-14 14:08:18 -0700

    data-structures

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+def toSubtract(list_num):
+    toSb = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            toSb += n
+
+    return (max_list - toSb)
+
+
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+
+    if not isinstance(roman_string, str):
+        return 0
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
+
+    number = 0
+    last_rom = 0
+    list_num = [0]
+
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    number += toSubtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
+
+                last_rom = rom_n.get(ch)
+
+    number += toSubtract(list_num)
+
+    return (number)
