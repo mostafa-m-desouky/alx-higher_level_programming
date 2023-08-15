11a4237c6b83ad80f754e05d68452347d4d40d5f
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-08-15 16:25:11 -0700

    task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS  first_table (
+-- creates a tabel first_table in current db
+-- does not fail if table exists
+CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256),
 	PRIMARY KEY(id)
