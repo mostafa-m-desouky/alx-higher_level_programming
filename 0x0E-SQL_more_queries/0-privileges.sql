56c0043a067fcd3735e72c575948c501d786864b
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-08-16 13:24:33 -0700

    my first commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- Lists all privileges of MySQL Users 'user_0d_1'
--- and 'user_0d_2' in 'localhost'.
-
+-- Script that lists all privileges of the MySQL sever
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
