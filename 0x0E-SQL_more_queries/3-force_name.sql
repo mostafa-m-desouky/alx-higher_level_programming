280a4699da3e0ea45412daedfae8bac7a97c953e
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-08-16 13:43:33 -0700

    my

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,6 +2,5 @@
 -- Query to create the table 'force_name' in MySQL server
 CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY(id)
+	name VARCHAR(256) NOT NULL
 );
