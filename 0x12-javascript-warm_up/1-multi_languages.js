844cc27452a36b089f705418fe2d2e1ec3efdf5e
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-09-11 10:46:25 -0700

    mostafa

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,6 @@
 
 //prints C is fun,Python is cool and JavaScript is amazing.
 
-console.log("C is fun\nPython is cool\nJavaScript is amazing");
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
