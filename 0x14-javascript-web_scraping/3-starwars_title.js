aad5efbd828cf613c597bfb82baec2573d67feec
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2023-10-24 03:55:03 -0700

    stars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-let url = `http://swapi.co/api/films/${process.argv[2]}`;
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+
+const movieId = process.argv[2];
+
+if (parseInt(movieId) < 8) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+  request(url, function (err, response, body) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(JSON.parse(body).title);
+  });
+}
